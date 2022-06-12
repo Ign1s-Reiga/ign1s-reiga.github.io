f7dcd63023f3b3324e34e9af5a5444b1cbc624cb
Reiga <remrem4862@gmail.com>
2022-06-13 02:07:28 +0900

    css fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { Avatar} from '@mui/material';
+import { Avatar } from '@mui/material';
 
 const Home: NextPage = () => {
     return (
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
                 <link rel={"icon"} href={"/icon.png"}/>
             </Head>
             <div className={styles.main}>
-                <Avatar alt={"Ign1s Reiga"} src={"/icon.png"} className={styles.avatar}/>
+                <Avatar alt={"Ign1s Reiga"} src={"/icon.png"} sx={{ width: 280, height: 280, mb: '2rem' }}/>
                 <h1>Hi, There👋</h1>
                 <h2>I&apos;m Ign1s Reiga</h2>
                 <p style={{ color: "gray" }}>デザインが思いつかないからこれで放置</p>
