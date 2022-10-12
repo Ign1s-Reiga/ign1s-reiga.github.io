4e5ab71689d97b0f2058b307bd5864e895e540c7
Ign1s_Reiga <64723970+Ign1s-Reiga@users.noreply.github.com>
2022-10-12 22:15:21 +0900

    Chore/issue-template (#4)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,7 @@ const globalStyle = css`
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
     min-height: 100vh;
+    max-width: 100%;
     background-image: linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 50%, #f3e7e9 100%);
   }
   
@@ -29,6 +30,7 @@ const App = ({ Component, pageProps }: AppProps) => {
         <Head>
           <title>Ign1s Reiga - Portfolio</title>
           <meta name='description' content="Ign1s-Reiga's portfolio"/>
+          <meta name="viewport" content="width=device-width,initial-scale=1"/>
           <link rel='icon' href='/icon.png'/>
         </Head>
         <Global styles={ globalStyle }/>
